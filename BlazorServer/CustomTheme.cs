fcb6642124fe147f4013b1503e7164744327dddf
Gustav <gustav_.91@hotmail.com>
2023-10-31 21:57:22 +0100

    Blazor Server with Mudblazor

---
+++ BlazorServer/CustomTheme.cs
@@ -0,0 +1,39 @@
+﻿using MudBlazor;
+
+namespace BlazorServer_MudBlazor;
+
+public static class CustomThemes
+{
+    public static MudTheme Default = new MudTheme()
+    {
+        Palette = new PaletteLight()
+        {
+            AppbarBackground = "#ffffff",
+            AppbarText = "#594ae2",
+            Secondary = "#1ec8a5",
+            SecondaryLighten = "#2adfbb",
+            SecondaryDarken = "#19a98c",
+            Tertiary = "#00FFFF ",
+            TertiaryLighten = "#E0FFFF ",
+            TertiaryDarken = "#008B8B",
+        },
+        PaletteDark = new PaletteDark()
+        {
+            Secondary = "#1ec8a5",
+            SecondaryLighten = "#2adfbb",
+            SecondaryDarken = "#19a98c",
+            Tertiary = "#00FFFF ",
+            TertiaryLighten = "#E0FFFF ",
+            TertiaryDarken = "#008B8B",
+        },
+        LayoutProperties = new LayoutProperties()
+        {
+            AppbarHeight = "40px",
+            DefaultBorderRadius = "0px",
+        },
+        Shadows = new Shadow(),
+        PseudoCss = new PseudoCss(),
+        Typography = new Typography(),
+        ZIndex = new ZIndex(),
+    };
+}
---
+++ BlazorServer/Shared/Constants/CommonConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorServer_MudBlazor.Shared.Constants;
+
+public static class CommonConstants
+{
+    public const string ApplicationName = "Collie";
+    public const string ApplicationVersion = "0.1";
+}
